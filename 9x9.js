d88a3e98f7ade27a76f06806a60051c19a365e86
alex <alex830216@gmail.com>
2021-07-09 15:03:29 +0800

    ---

--- 9x9.js
+++ 9x9.js
@@ -2,6 +2,8 @@ for (var i = 1; i < 10; i++) {
     for (var j = 1; j < 10; j++) {
 
         // console.log(i + " x " + j + " = " + i * j )
+
+				
 				console.log(`${i} x ${j} = ${i * j}`)
 
     }
--- no46.js
+++ no46.js
@@ -1,7 +1,8 @@
 for (var i = 1; i < 10; i++) {
     // 不能印出4跟6
-  if (i == 4 || i == 6) {
-    continue;
-  }
+  // if (i == 4 || i == 6) {
+  //   continue;
+  // }
+  
   console.log(i)
 }
