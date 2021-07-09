61dfeb11e33aaaf3d829401739076dba97f43249
alex <alex830216@gmail.com>
2021-07-09 11:58:11 +0800

    array

---
+++ array-add&delete-items.js
@@ -0,0 +1,7 @@
+let items = ["a", 7, "c", "d"]
+// 加在最後面
+items.push("e")
+// 加在最前面
+items.unshift(5)
+items.shift()
+console.log(items)
---
+++ array.js
@@ -0,0 +1,5 @@
+let items = ["a", 7, "c", "d"]
+console.log(items[0])
+console.log(items[1])
+console.log(items[2])
+console.log(items[3])
