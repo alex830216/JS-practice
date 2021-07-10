8e548a391930229557dda99affcd4a275867ac6e
alex <alex830216@gmail.com>
2021-07-10 17:35:10 +0800

    JS資料型態

---
+++ type.js
@@ -0,0 +1,15 @@
+console.log(typeof true);           // 'boolean'
+console.log(typeof 'Kuro');         // 'string'
+console.log(typeof 123);            // 'number'
+console.log(typeof { });            // 'object'
+console.log(typeof [ ]);            // 'object'
+console.log(typeof console);        // 'object'
+console.log(typeof console.log);    // 'function'
+console.log(typeof null);           // 'object'
+var a = null;
+console.log(a);                     // 'null'
+// var b = Null;
+// console.log(b);                  // 'null'
+var valueUndefined;                 
+console.log(valueUndefined);        // 'undefined'
+console.log(typeof undefined);      // 'undefined'
