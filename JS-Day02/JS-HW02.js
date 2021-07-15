fa88b4b1c9f65ac25930d303aa712bd19c6cc2a0
alex <alex830216@gmail.com>
2021-07-15 20:50:01 +0800

    修正作業02

--- JS-Day02/JS-HW02.js
+++ JS-Day02/JS-HW02.js
@@ -1,7 +1,7 @@
   // BMI值計算公式: BMI = 體重(公斤) / 身高2(公尺2)
 
 function bmiCalculator(height, weight) {
-    return Math.floor(weight / (height * height / 10000) * 10) / 10
+    return Math.floor(weight / ((height / 100) ** 2) * 10) / 10
 }    
   
   // 身高 170 公分、體重 65 公斤
