11f8aa4a4c2192eee89290cca69c5e6159309919
alex <alex830216@gmail.com>
2021-07-15 20:57:59 +0800

    function簡單練習-轉換成箭頭

---
+++ prac.js
@@ -0,0 +1,13 @@
+for (var i = 0; i < 3; i++) {
+    setTimeout(() => console.log(i), 1);
+  }
+ 
+  // 注意有兩個參數!!!
+  // 第一個參數 () => console.log(i)
+  // 第二個參數 1
+  // 重寫一次
+ for (var i = 0; i < 3; i++) {
+   const setTimeout = () => {
+   console.log(i);
+   }, 1);  
+ }
