d72674cf7a926574ab1b3f8e61112075f6f925f3
alex <alex830216@gmail.com>
2021-07-15 20:49:33 +0800

    函式箭頭寫法

--- JS-Day02/c-turn-f.js
+++ JS-Day02/c-turn-f.js
@@ -2,8 +2,23 @@
 // 華氏 f = (攝氏) x (9/5) + 32
 
 
-function c2f(t) {
-   return t * (9/5) + 32
-  }
+// function c2f(t) {
+//    return t * (9/5) + 32
+// }
   
-  console.log(`85度 C = ${c2f(85)}度 F`) 
+// console.log(`85度 C = ${c2f(85)}度 F`) 
+
+// const c2f = (t) =>  t * (9/5) + 32
+  
+// console.log(`85度 C = ${c2f(85)}度 F`) 
+
+
+// for (var i = 0; i < 3; i++) {
+//    setTimeout(() => console.log(i), 1);
+//  }
+
+for (var i = 0; i < 3; i++) {
+  const setTimeout = function() {
+  console.log(i), 1);
+}
+}
