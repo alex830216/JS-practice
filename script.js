53c34c81868891f3306bb024146fcf90d39a4bde
alex <alex830216@gmail.com>
2021-07-17 16:06:26 +0800

    計時器作業

---
+++ script.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const btn = document.querySelector("#start-btn")
+    btn.addEventListener("click", () => {
+        let i = 0
+        setInterval(() => {
+            const changetime = document.querySelector("#timer")
+            i = i + 1
+            changetime.innerText = i
+        }, 1000)
+    })
+})
