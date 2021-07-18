bd1bdf92824c9dd0182f8492a9374d109c8a083d
alex <alex830216@gmail.com>
2021-07-18 20:39:33 +0800

    移動

--- JS-Day04/script.js
+++ JS-Day04/script.js
@@ -1,24 +1,11 @@
-    // addEventListener(事件,做啥)
-    document.addEventListener("DOMContentLoaded", () => {
-
-    // 三秒後換字
-    // 有點囉嗦，可以把function寫進去setTimeout就好
-    // const h = document.querySelector("#hi")
-    // function changenum(){
-    //     h.innerHTML = "456"
-    // }
-    // 這樣比較好
-    // setTimeout(() => {h.innerHTML = "456"}, 3000);
-
-    // 按完按鈕換字
-    const btn = document.querySelector("#btn")
-    
+document.addEventListener("DOMContentLoaded", () => {
+    const btn = document.querySelector("#start-btn")
     btn.addEventListener("click", () => {
-        const h = document.querySelector("#hi")
-        h.innerHTML = "被按了"
-        // h.innerText = "被按了"
+        let i = 0
+        setInterval(() => {
+            const changetime = document.querySelector("#timer")
+            i = i + 1
+            changetime.innerText = i
+        }, 1000)
     })
-
-
-
 })
