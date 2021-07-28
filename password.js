69ae89f337a0427cd5b8d49cef7b779af0b3bad2
alex <alex830216@gmail.com>
2021-07-28 23:30:45 +0800

    密碼輸入限制

---
+++ password.js
@@ -0,0 +1,20 @@
+const isValidPassword = (password) => {
+    // 規則：
+    // 1. 密碼至少要 8 個字
+    // 2. 密碼裡至少要有 1 個數字以及 1 個大寫英文字母
+    // 3. 密碼裡不能有 "password" 字樣
+  if(password.length >= 8 && /[A-Z]/.test(password) && /\d/.test(password) ) {
+    return true
+  } else if(/password/.test(password)) {
+    return false
+  } else {
+    return false
+  }
+
+}
+  
+console.log(isValidPassword("12345"));           // false
+console.log(isValidPassword("123passwordxyz"));  // false
+console.log(isValidPassword("helloworld"));      // false
+console.log(isValidPassword("Helloworld"));      // false
+console.log(isValidPassword("helloWorld2"));     // true
